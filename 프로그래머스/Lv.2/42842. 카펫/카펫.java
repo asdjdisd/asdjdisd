b48394307ab96552d201057180d01826c6e9e8f7
asdjdisd <126261792+asdjdisd@users.noreply.github.com>
2024-01-13 13:48:52 +0900

    [level Lv.2] Title: 카펫, Time: 0.02 ms, Memory: 72.8 MB -BaekjoonHub

--- 프로그래머스/Lv.2/42842. 카펫/카펫.java
+++ 프로그래머스/Lv.2/42842. 카펫/카펫.java
@@ -3,14 +3,15 @@ class Solution {
     public int[] solution(int brown, int yellow) {
         int sum=brown+yellow;
         int[] answer=new int[2];
+        loop:
         for(int q=3;q<brown;q++)
-            for(int w=3;w<brown;w++)
-                if(sum==q*w)
-                    if(brown==(q*2+w*2)-4)
-                    {
-                        answer[0]=q;
-                        answer[1]=w;
-                    }
+            for(int w=q;w<brown;w++)
+                if(sum==q*w&&brown==(q*2+w*2)-4)
+                {
+                    answer[0]=w;
+                    answer[1]=q;
+                    break loop;
+                }      
         return answer;
     }
 }
