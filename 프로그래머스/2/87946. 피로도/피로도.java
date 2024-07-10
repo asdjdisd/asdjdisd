cc75bf32d9d04b3c22d981b1ca0506dc87ba35ed
asdjdisd <126261792+asdjdisd@users.noreply.github.com>
2024-07-10 17:47:56 +0900

    [level 2] Title: 피로도, Time: 0.05 ms, Memory: 83.4 MB -BaekjoonHub

---
+++ 프로그래머스/2/87946. 피로도/피로도.java
@@ -0,0 +1,24 @@
+import java.util.*;
+class Solution {
+    int answer=0;
+    public void dfs(int k, int[][] dun, boolean[] check,int n)
+    {
+
+        for(int q=0;q<dun.length;q++)
+        {
+            if(!check[q]&&k>=dun[q][0])
+            {
+                check[q]=true;
+                dfs(k-dun[q][1],dun,check,n+1);
+                check[q]=false;
+            }
+        }
+        answer=Math.max(answer,n);
+    }
+    public int solution(int k, int[][] dungeons) {
+
+        boolean[] check=new boolean[dungeons.length];
+        dfs(k,dungeons,check,0);
+        return answer;
+    }
+}
