690410678b6f87bd27ea27bc83fb8b20869d1ec0
asdjdisd <126261792+asdjdisd@users.noreply.github.com>
2024-07-31 20:50:50 +0900

    [level 3] Title: 파괴되지 않은 건물, Time: 57.61 ms, Memory: 214 MB -BaekjoonHub

--- 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
+++ 프로그래머스/3/92344. 파괴되지 않은 건물/파괴되지 않은 건물.java
@@ -10,31 +10,25 @@ class Solution {
                 check=-1;
             else
                 check=1;
-            
+             
             int a=skill[q][1];
             int b=skill[q][2];
             int c=skill[q][3]+1;
             int d=skill[q][4]+1;
-            
+            //누적합 (x1, y1) ~ (x2, y2)까지에 n만큼의 변화를 주고 싶다면,
+            //(x1, y1) = n, (x2+1, y1) = -n, (x1, y2+1) = -n, (x2+1, y2+1) = n 만큼의 값을 더해줌
             num[a][b]+=check*skill[q][5];
             num[c][b]+=check*skill[q][5]*-1;
             num[a][d]+=check*skill[q][5]*-1;
             num[c][d]+=check*skill[q][5];
         }
-        for(int q=0;q<num.length;q++)
-        {
+        
+        for(int q=0;q<num.length;q++)//가로 누적합
             for(int w=1;w<num[0].length;w++)
-            {
                 num[q][w]+=num[q][w-1];
-            }
-        }
-        for(int q=1;q<num.length;q++)
-        {
+        for(int q=1;q<num.length;q++)//세로 누적합
             for(int w=0;w<num[0].length;w++)
-            {
                 num[q][w]+=num[q-1][w];
-            }
-        }
         
         for(int q=0;q<board.length;q++)
         {
