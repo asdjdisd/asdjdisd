70db5584ccc06f8af36de747f8784921720bb91f
asdjdisd <126261792+asdjdisd@users.noreply.github.com>
2024-09-14 20:39:15 +0900

    [level 2] Title: [PCCP 기출문제] 3번 / 충돌위험 찾기, Time: 565.70 ms, Memory: 814 MB -BaekjoonHub

---
+++ 프로그래머스/2/340211. ［PCCP 기출문제］ 3번 ／ 충돌위험 찾기/［PCCP 기출문제］ 3번 ／ 충돌위험 찾기.java
@@ -0,0 +1,65 @@
+import java.util.*;
+class Solution {
+    public int solution(int[][] points, int[][] routes) {
+        int answer = 0;
+        int maxx=0;
+        int maxy=0;
+        for(int q=0;q<points.length;q++)
+        {
+            maxx=Math.max(maxx,points[q][0]);
+            maxy=Math.max(maxy,points[q][1]);
+        }
+        int[][][] map=new int[maxx+1][maxy+1][15000];
+        
+        for(int q=0;q<routes.length;q++)
+        {
+            int check=0;
+            int ax;
+            int ay;
+            int bx;
+            int by;
+            for(int w=1;w<routes[q].length;w++)
+            {
+                ax=points[routes[q][w-1]-1][0];
+                ay=points[routes[q][w-1]-1][1];
+                bx=points[routes[q][w]-1][0];
+                by=points[routes[q][w]-1][1];
+                
+                if(check==0)
+                    map[ax][ay][check]++;
+                
+                while(ax!=bx||ay!=by)
+                {
+                    if(ax!=bx)
+                    {
+                        if(bx>ax)
+                            ax++;
+                        else
+                            ax--;
+                    }
+                    else
+                    {
+                        if(by>ay)
+                            ay++;
+                        else
+                            ay--;
+                    }
+                    check++;
+                    map[ax][ay][check]++;
+                }
+            }
+        }
+        for(int q=0;q<map.length;q++)
+        {
+            for(int w=0;w<map[0].length;w++)
+            {
+                for(int e=0;e<map[0][0].length;e++)
+                {
+                    if(map[q][w][e]>=2)
+                        answer++;
+                }
+            }
+        }
+        return answer;
+    }
+}
